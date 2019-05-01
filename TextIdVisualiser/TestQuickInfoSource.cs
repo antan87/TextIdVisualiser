2aa4541e7fb9d2e40c9b217cb9caa0ecca3a0f41
Anton Patron <anton.patron@aareon.com>
2019-05-01 12:22:26 +0200

    Clean up

--- TextIdVisualiser/TestQuickInfoSource.cs
+++ TextIdVisualiser/TestQuickInfoSource.cs
@@ -1,79 +1,43 @@
-﻿using Microsoft.VisualStudio.Language.Intellisense;
-using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Operations;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
 
-namespace TextIdVisualiser
-{
-    internal class TestQuickInfoSource : IAsyncQuickInfoSource
-    {
+namespace TextIdVisualiser {
+    internal class TestQuickInfoSource : IAsyncQuickInfoSource {
         private readonly TestQuickInfoSourceProvider m_provider;
         private readonly ITextBuffer m_subjectBuffer;
         private readonly Dictionary<string, string> m_dictionary;
         private bool m_isDisposed;
-
-        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> qiContent, out ITrackingSpan applicableToSpan)
-        {
-            // Map the trigger point down to our buffer.
-            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(this.m_subjectBuffer.CurrentSnapshot);
-            if (!subjectTriggerPoint.HasValue)
-            {
-                applicableToSpan = null;
-                return;
-            }
-
-            ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
-
-            //look for occurrences of our QuickInfo words in the span
-            ITextStructureNavigator navigator = this.m_provider.NavigatorService.GetTextStructureNavigator(this.m_subjectBuffer);
-            TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
-            string searchText = extent.Span.GetText();
-            if (this.m_dictionary.TryGetValue(searchText, out string value))
-            {
-                applicableToSpan = currentSnapshot.CreateTrackingSpan(extent.Span.Start, searchText.Length, SpanTrackingMode.EdgeInclusive);
-                qiContent.Add(value);
-            }
-            else
-                applicableToSpan = null;
-        }
-
-        public void Dispose()
-        {
-            if (!this.m_isDisposed)
-            {
-                GC.SuppressFinalize(this);
+        public void Dispose () {
+            if (!this.m_isDisposed) {
+                GC.SuppressFinalize (this);
                 this.m_isDisposed = true;
             }
         }
-
-
-        public Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken)
-        {
-            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(this.m_subjectBuffer.CurrentSnapshot);
+        public Task<QuickInfoItem> GetQuickInfoItemAsync (IAsyncQuickInfoSession session, CancellationToken cancellationToken) {
+            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint (this.m_subjectBuffer.CurrentSnapshot);
             if (!subjectTriggerPoint.HasValue)
-                return Task.FromResult(new QuickInfoItem(null, null));
+                return Task.FromResult (new QuickInfoItem (null, null));
 
             ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
 
             //look for occurrences of our QuickInfo words in the span
-            ITextStructureNavigator navigator = this.m_provider.NavigatorService.GetTextStructureNavigator(this.m_subjectBuffer);
-            TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
-            string searchText = extent.Span.GetText();
-            if (this.m_dictionary.TryGetValue(searchText, out string value))
-            {
+            ITextStructureNavigator navigator = this.m_provider.NavigatorService.GetTextStructureNavigator (this.m_subjectBuffer);
+            TextExtent extent = navigator.GetExtentOfWord (subjectTriggerPoint.Value);
+            string searchText = extent.Span.GetText ();
+            if (this.m_dictionary.TryGetValue (searchText, out string value)) {
 
-                var applicableToSpan = currentSnapshot.CreateTrackingSpan(extent.Span.Start, searchText.Length, SpanTrackingMode.EdgeInclusive);
-                return Task.FromResult(new QuickInfoItem(applicableToSpan, value));
-            }
-            else
-                return Task.FromResult(new QuickInfoItem(null, searchText));
+                var applicableToSpan = currentSnapshot.CreateTrackingSpan (extent.Span.Start, searchText.Length, SpanTrackingMode.EdgeInclusive);
+                return Task.FromResult (new QuickInfoItem (applicableToSpan, value));
+            } else
+                return Task.FromResult (new QuickInfoItem (null, searchText));
         }
 
-        public TestQuickInfoSource(Dictionary<string, string> values, TestQuickInfoSourceProvider provider, ITextBuffer subjectBuffer)
-        {
+        public TestQuickInfoSource (Dictionary<string, string> values, TestQuickInfoSourceProvider provider, ITextBuffer subjectBuffer) {
             this.m_provider = provider;
             this.m_subjectBuffer = subjectBuffer;
 
--- TextIdVisualiser/TestQuickInfoSourceProvider.cs
+++ TextIdVisualiser/TestQuickInfoSourceProvider.cs
@@ -1,33 +1,24 @@
-﻿using Microsoft.VisualStudio.Language.Intellisense;
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Utilities;
-using System;
-using System.ComponentModel.Composition;
 
-namespace TextIdVisualiser
-{
-    [Export(typeof(IAsyncQuickInfoSourceProvider))]
-    [Name("ToolTip QuickInfo Source")]
-    [Order(Before = "Default Quick Info Presenter")]
-    [ContentType("text")]
-    internal class TestQuickInfoSourceProvider : IAsyncQuickInfoSourceProvider
-    {
+namespace TextIdVisualiser {
+    [Export (typeof (IAsyncQuickInfoSourceProvider))]
+    [Name ("ToolTip QuickInfo Source")]
+    [Order (Before = "Default Quick Info Presenter")]
+    [ContentType ("text")]
+    internal class TestQuickInfoSourceProvider : IAsyncQuickInfoSourceProvider {
 
         [Import]
         internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
 
         [Import]
         internal ITextBufferFactoryService TextBufferFactoryService { get; set; }
-
-        //public IAsyncQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
-        //{
-        //    return new TestQuickInfoSource(ResourceManagerHelper.TextValues, this, textBuffer);
-        //}
-
-        IAsyncQuickInfoSource IAsyncQuickInfoSourceProvider.TryCreateQuickInfoSource(ITextBuffer textBuffer)
-        {
-            return new TestQuickInfoSource(ResourceManagerHelper.TextValues, this, textBuffer);
+        IAsyncQuickInfoSource IAsyncQuickInfoSourceProvider.TryCreateQuickInfoSource (ITextBuffer textBuffer) {
+            return new TestQuickInfoSource (ResourceManagerHelper.TextValues, this, textBuffer);
         }
     }
 }
